da2930a0f96303cc66b7cae655fbf7ea8e1ba6b0
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-26 16:07:07 +0100

    Day 1 - oca

---
+++ Day1/Test.java
@@ -0,0 +1,12 @@
+public class Test{
+	
+	public static void main(String tab[])
+	
+	{
+		    //System.out.println("Hello Java OCA"); // System.out.println : Afficher sur la console
+			System.out.println(tab[0]);  // premier paramètre
+			System.out.println(tab[1]);  // deuxième paramètre
+			System.out.println(tab[2]);
+	
+	}
+}
